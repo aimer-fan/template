b53208f2201be508d68da7b987bb0f5744f7e01b
aimer-fan <1261105760@qq.com>
2025-05-08 23:15:01 +0800

    feat: add vite react ts template

---
+++ vite-react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Vite React App</h1>
+      <p>This is a basic React application using Vite and TypeScript.</p>
+    </div>
+  );
+};
+
+export default App;
